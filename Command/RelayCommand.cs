ee56dd0191e57d7881f0cb1797a338fbf1807aa2
Leo <77863733+leofox2021@users.noreply.github.com>
2023-06-10 03:13:29 +0400

    write relay command & fix startupUri

--- Command/RelayCommand.cs
+++ Command/RelayCommand.cs
@@ -5,16 +5,19 @@ namespace WPFMusicPlayer.Command
 {
     public class RelayCommand : ICommand
     {
-        public bool CanExecute(object parameter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Execute(object parameter)
-        {
-            throw new NotImplementedException();
-        }
-
         public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> executeMethod, Predicate<object> canExecuteMethod)
+        {
+            ExecuteMethod = executeMethod;
+            CanExecuteMethod = canExecuteMethod;
+        }
+        
+        public Action<object> ExecuteMethod { get; set; }
+        public Predicate<object> CanExecuteMethod { get; set; }
+
+        public bool CanExecute(object parameter) => CanExecuteMethod(parameter);
+
+        public void Execute(object parameter) => ExecuteMethod(parameter);
     }
 }
