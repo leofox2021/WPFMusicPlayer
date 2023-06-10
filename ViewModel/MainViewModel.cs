a468a0eb00efb5ec3c5c6d2ac41809471b8becce
Leo <77863733+leofox2021@users.noreply.github.com>
2023-06-10 19:14:17 +0400

    bind selected row in listview to a property in viewmodel

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using System.Windows.Media;
 using WPFMusicPlayer.Command;
 using WPFMusicPlayer.Model;
 
 namespace WPFMusicPlayer.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        private int _selectedSongIndex;
+        
         public MainViewModel()
         {
             Songs = SongController.Songs;
@@ -15,16 +21,16 @@ namespace WPFMusicPlayer.ViewModel
             AddSongCommand = new RelayCommand(AddSong, CanAddSong);
         }
 
-        private bool CanAddSong(object obj)
+        public int SelectedSongIndex
         {
-            return true;
+            get => _selectedSongIndex; 
+            set
+            {
+                _selectedSongIndex = value;
+                OnPropertyChanged();
+            }
         }
-
-        private void AddSong(object obj)
-        {
-            SongController.AddSong();
-        }
-
+        
         public ObservableCollection<Song> Songs { get; set; }
         
         public ICommand AddSongCommand { get; set; }
@@ -34,10 +40,18 @@ namespace WPFMusicPlayer.ViewModel
         public ICommand NextSongCommand { get; set; }
         public ICommand PreviousSongCommand { get; set; }
         
-        private void PlaySong(object obj)
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        
+        private bool CanAddSong(object obj) => true;
+
+        private void AddSong(object obj) => SongController.AddSong();
+
+        private void PlaySong(object obj) => MusicPlayer.Play();
 
         private bool CanPlaySong(object obj) => true;
     }
